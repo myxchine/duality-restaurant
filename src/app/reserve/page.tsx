d59b59d6c4c1dc4ff0638008c8ff895edcdd1557
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-05-30 19:19:24 +0100

    t

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import ReservationForm, {
-  ReservationFormData,
-} from "@/reserve/form";
-import Header from "@/components/elements/reserve/header";
-import Footer from "@/components/elements/reserve/footer";
+import ReservationForm, { ReservationFormData } from "@/reserve/form";
 
 const ReservationPage: React.FC = () => {
   const handleFormSubmit = (formData: ReservationFormData) => {
@@ -12,16 +8,12 @@ const ReservationPage: React.FC = () => {
   };
 
   return (
-    <>
-      <Header />
-      <main className="flex flex-col items-center justify-center h-max p-12 ">
-        <div className=" rounded-lg p-8 shadow-md space-y-8 w-full max-w-lg   bg-white bg-opacity-90">
-          <h1 className="text-2xl font-bold">Make a Reservation</h1>
-          <ReservationForm onSubmit={handleFormSubmit} />
-        </div>
-      </main>
-      <Footer />
-    </>
+    <main className="flex flex-col items-center justify-center h-screen p-12 ">
+      <div className=" rounded-lg p-8 shadow-md space-y-8 w-full max-w-lg   bg-white bg-opacity-90">
+        <h1 className="text-2xl font-bold">Make your reservation</h1>
+        <ReservationForm onSubmit={handleFormSubmit} />
+      </div>
+    </main>
   );
 };
 
